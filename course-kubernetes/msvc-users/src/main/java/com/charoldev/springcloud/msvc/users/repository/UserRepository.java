2345360e08d5d5de05c56e86ab59f13ca4aa86f1
Harold Rojas <harolrop14@gmail.com>
2022-07-05 17:38:54 -0500

    extend crudRepo

--- course-kubernetes/msvc-users/src/main/java/com/charoldev/springcloud/msvc/users/repository/UserRepository.java
+++ course-kubernetes/msvc-users/src/main/java/com/charoldev/springcloud/msvc/users/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.charoldev.springcloud.msvc.users.repository;
 
-public interface UserRepository {
+import com.charoldev.springcloud.msvc.users.model.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    
 }
