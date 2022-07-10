dbe1dc99ccf5468f9542345b8d62d5b8e7c75856
charold-legion-s7 <harolrop14@gmail.com>
2022-07-10 16:54:20 -0500

    assignuser method

--- course-kubernetes/msvc-courses/src/main/java/com/charoldev/springcloud/msvc/courses/services/CourseService.java
+++ course-kubernetes/msvc-courses/src/main/java/com/charoldev/springcloud/msvc/courses/services/CourseService.java
@@ -1,17 +1,16 @@
 package com.charoldev.springcloud.msvc.courses.services;
 
+import com.charoldev.springcloud.msvc.courses.models.User;
 import com.charoldev.springcloud.msvc.courses.models.entity.Course;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CourseService {
-
     List<Course> findAll();
-
     Optional<Course> findById(Long id);
-
     Course save(Course course);
-
     void delete(Long id);
+//    Optional<User> assignUser(User user, Long id);
+
 }
