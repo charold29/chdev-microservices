dd3b2994f652d5990b137d2d85b0e47fc990df2c
Harold Rojas <harolrop14@gmail.com>
2022-07-13 15:21:32 -0500

    implementacion de metodos de service course

--- course-kubernetes/msvc-courses/src/main/java/com/charoldev/springcloud/msvc/courses/services/CourseServiceImpl.java
+++ course-kubernetes/msvc-courses/src/main/java/com/charoldev/springcloud/msvc/courses/services/CourseServiceImpl.java
@@ -3,6 +3,7 @@ package com.charoldev.springcloud.msvc.courses.services;
 import com.charoldev.springcloud.msvc.courses.interfaces.UserClientRest;
 import com.charoldev.springcloud.msvc.courses.models.User;
 import com.charoldev.springcloud.msvc.courses.models.entity.Course;
+import com.charoldev.springcloud.msvc.courses.models.entity.CourseUser;
 import com.charoldev.springcloud.msvc.courses.repositories.CourseRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -42,17 +43,56 @@ public class CourseServiceImpl implements CourseService{
     }
 
     @Override
+    @Transactional
     public Optional<User> assignUser(User user, Long courseId) {
+        Optional<Course> courseOptional = courseRepository.findById(courseId);
+        if (courseOptional.isPresent()){
+            User userMicroservice = clientRest.getUserById(user.getId());
+
+            Course course = courseOptional.get();
+            CourseUser courseUser = new CourseUser();
+            courseUser.setUserId(userMicroservice.getId());
+
+            course.addCourseUser(courseUser);
+            courseRepository.save(course);
+            return Optional.of(userMicroservice);
+        }
         return Optional.empty();
     }
 
     @Override
+    @Transactional
     public Optional<User> createUser(User user, Long courseId) {
+        Optional<Course> courseOptional = courseRepository.findById(courseId);
+        if (courseOptional.isPresent()){
+            User newUserMicroservice = clientRest.createUser(user);
+
+            Course course = courseOptional.get();
+            CourseUser courseUser = new CourseUser();
+            courseUser.setUserId(newUserMicroservice.getId());
+
+            course.addCourseUser(courseUser);
+            courseRepository.save(course);
+            return Optional.of(newUserMicroservice);
+        }
         return Optional.empty();
     }
 
     @Override
+    @Transactional
     public Optional<User> unassignUser(User user, Long courseId) {
+        Optional<Course> courseOptional = courseRepository.findById(courseId);
+        if (courseOptional.isPresent()){
+            User userMicroservice = clientRest.getUserById(user.getId());
+
+            Course course = courseOptional.get();
+            CourseUser courseUser = new CourseUser();
+            courseUser.setUserId(userMicroservice.getId());
+
+            course.removeCourseUser(courseUser);
+            courseRepository.save(course);
+            return Optional.of(userMicroservice);
+        }
         return Optional.empty();
     }
 
