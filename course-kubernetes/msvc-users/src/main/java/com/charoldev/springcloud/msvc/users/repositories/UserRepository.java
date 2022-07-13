96b80546484b1c36fe51e429b7dc06bf99e46d9f
Harold Rojas <harolrop14@gmail.com>
2022-07-13 16:27:30 -0500

    query findByEmail method comment

--- course-kubernetes/msvc-users/src/main/java/com/charoldev/springcloud/msvc/users/repositories/UserRepository.java
+++ course-kubernetes/msvc-users/src/main/java/com/charoldev/springcloud/msvc/users/repositories/UserRepository.java
@@ -1,7 +1,7 @@
 package com.charoldev.springcloud.msvc.users.repositories;
 
 import com.charoldev.springcloud.msvc.users.models.entity.User;
-import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
@@ -9,6 +9,10 @@ import java.util.Optional;
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
-    @Query("select u from User u where u.email=?1")
-    Optional<User> byEmail(String email);
+    /*Find By Email Query Method:
+
+      @Query("select u from User u where u.email=?1")
+      Optional<User> findByEmail(String email);
+     */
+
 }
